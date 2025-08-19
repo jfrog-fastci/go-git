1fa47cb575169c115b9ae992979a1d5992750e91
Yahav Ohana <yahavo@jfrog.com>
2025-08-19 14:57:33 +0000

    Test new way of building images

--- docker-base-image/build-docker.sh
+++
@@ -1,3 +0,0 @@
-docker build --network=none -t trialrrdq7a.jfrog.io/docker-trial/go-ci-base:1.24-warm -f Dockerfile .
-
-# https://trialrrdq7a.jfrog.io/artifactory/docker-trial/
---
+++ docker-base-image/build.baseimage.sh
@@ -0,0 +1,3 @@
+docker build --network=none -t trialrrdq7a.jfrog.io/docker-trial/go-git:latest -f Dockerfile.baseimage .
+
+# https://trialrrdq7a.jfrog.io/artifactory/docker-trial/
